af91c8cfebac57f2cb45c2e2583b5e4beaf742e8
olmartin <olmartin@student.42lausanne.ch>
2022-11-24 16:49:57 +0100

    ex00 mod OK

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,6 +3,29 @@
 #include <iostream>
 #include "whatever.hpp"
 
+int	main( void ) {
+
+	int a = 2;
+	int b = 3;
+
+	::swap( a , b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl; 
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl; 
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap( c , d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl; 
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl; 
+
+	return 0;
+}
+
+
+/*
 int	main( void )
 {
 	int	a = 22;
@@ -37,4 +60,4 @@ int	main( void )
 
 	return 0;
 }
-
+*/
