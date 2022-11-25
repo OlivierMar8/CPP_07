4541b497a26951faae20ba3dd7753e26d02eab6e
olmartin <olmartin@student.42lausanne.ch>
2022-11-25 16:07:52 +0100

    essais 08 OK

--- ex01/test0701_08main.cpp
+++ ex01/test0701_08main.cpp
@@ -6,7 +6,7 @@
 /*   By: olmartin <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 14:35:35 by olmartin          #+#    #+#             */
-/*   Updated: 2022/11/25 15:59:24 by olmartin         ###   ########.fr       */
+/*   Updated: 2022/11/25 16:04:46 by olmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 #include <iostream>
 
 template< typename U>
-void	print_value(U a) {
-	std::cout << "test06: " << a << std::endl;
+void	print_value(U a, size_t i) {
+	std::cout << "test06: " << a << " at position " << i << std::endl;
 	
 }
 
 template< typename T>
-void    iter( T * ptrT, size_t size, void(*p_func)(T))
+void    iter( T * ptrT, size_t size, void(*p_func)(T, size_t))
 {
     for (size_t i = 0; i < size; i++ )
     {
-		p_func(ptrT[i]);
+		p_func(ptrT[i], i);
     }
 }
 
